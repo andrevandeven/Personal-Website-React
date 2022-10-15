9a72fe04696272d29d8e8988502bcbea057831c2
andrevandeven <andrev@sas.upenn.edu>
2022-10-14 21:34:45 -0400

    recreated website using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="page-container">
+      <div className="page-header">
+        <h1>Andre van de Ven</h1>
+        <h2>UPenn 2026</h2>
+      </div>
+      <div className="info-container">
+        <div className="top-pics">
+          <img src="andre-diving.jpeg" alt="andre"/>
+        </div>
+        <div className="top-pics">
+          <img src="IMG_4760.jpg" alt="andre1"/>
+        </div>
+        <div className="personalInfo">
+          <div class="infoText">
+          <p>Hi! I'm a freshman at Penn studying neuroscience.</p>
+          <p>My hobbies include:</p>
+          <ul>
+            <li>Scuba diving</li>
+            <li>Underwater photography</li>
+            <li>Reading</li>
+            <li>Playing video games</li>
+          </ul>
+          </div>
+          <div className="bottom-pic">
+            <img src="IMG_9216.jpeg" alt="andre-and-ana"/>
+          </div>
+        </div>
+      </div>
+        <a className="links" href="https://www.andrevandeven.us/">
+          Photography Website
         </a>
-      </header>
+        <a className="links" href="https://github.com/andrevandeven">
+          GitHub
+        </a>
     </div>
   );
 }
